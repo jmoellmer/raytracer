7bd90dcf63504f7563cb2a1e2d7ef81a061e791c
Jeff Moellmer <jeff.moellmer@gmail.com>
2024-06-04 17:27:30 -0500

    Implemented scaling

--- matrices/transforms.py
+++ matrices/transforms.py
@@ -3,8 +3,54 @@ from .matrix import Matrix
 class Transform:
 
     @classmethod
-    def translation(cls, x, y, z):
+    def translate(cls, x, y, z):
+        """translate is a transformation that moves a point.
+
+        If you take the inverse of a translation matrix, you
+        get another translation matrix that moves a point in
+        reverse. Multiplying a translation matrix by a vector
+        should not change a vector.
+
+        Parameters
+        ----------
+        x : float
+            Amount to translate in the x-direction
+        y : float
+            Amount to translate in the y-direction
+        z : float
+            Amount to translate in the z-direction
+        
+
+        Returns
+        -------
+        Matrix
+            A 4x4 translation matrix
+        """
         return Matrix([[1, 0, 0, x],
                        [0, 1, 0, y],
                        [0, 0, 1, z],
+                       [0, 0, 0, 1]])
+    
+    @classmethod
+    def scale(cls, x, y, z):
+        """Scale "moves" by multiplication, scaling all points
+        on the object, effectively making it larger or smaller.
+
+        Parameters
+        ----------
+        x : float
+            Amount to scale x
+        y : float
+            Amount to scale y
+        z : float
+            amount to scale z
+        
+        Returns
+        -------        
+        Matrix
+            A 4x4 scaling matrix
+        """
+        return Matrix([[x, 0, 0, 0],
+                       [0, y, 0, 0],
+                       [0, 0, z, 0],
                        [0, 0, 0, 1]])
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -5,17 +5,38 @@ from tuples.point import Point
 from tuples.vector import Vector
 
 def test_Multiplying_by_a_translation_matrix():
-    transform = Transform.translation(5, -3, 2)
+    transform = Transform.translate(5, -3, 2)
     p = Point(-3, 4, 5)
     assert transform * p == Point(2, 1, 7)
 
 def test_Multiplying_by_the_inverse_of_a_translation_matrix():
-    transform = Transform.translation(5, -3, 2)
+    transform = Transform.translate(5, -3, 2)
     inv = transform.inverse()
     p = Point(-3, 4, 5)
     assert inv * p == Point(-8, 7, 3)
 
 def test_Translation_does_not_affect_vectors():
-    transform = Transform.translation(5, -3, 2)
+    transform = Transform.translate(5, -3, 2)
     v = Vector(-3, 4, 5)
     assert transform * v == v
+
+def test_A_scaling_matrix_applied_to_a_point():
+    transform = Transform.scale(2, 3, 4)
+    p = Point(-4, 6, 8)
+    assert transform * p == Point(-8, 18, 32)
+
+def test_A_scaling_matrix_applied_to_a_vector():
+    transform = Transform.scale(2, 3, 4)
+    v = Vector(-4, 6, 8)
+    assert transform * v == Vector(-8, 18, 32)
+
+def test_Multiplying_by_the_inverse_of_a_scaling_matrix():
+    transform = Transform.scale(2, 3, 4)
+    inv = transform.inverse()
+    v = Vector(-4, 6, 8)
+    assert inv * v == Vector(-2, 2, 2)
+
+def test_Reflection_is_scaling_by_a_negative_value():
+    transform = Transform.scale(-1, 1, 1)
+    p = Point(2, 3, 4)
+    assert transform * p == Point(-2, 3, 4)
